40e7daf8cd4bef74361ee7897c753136956a98f0
lokeshyaragarla <lokeshyaragarla@gmail.com>
2025-06-18 08:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-() => { 
-    cy.visit(baseUrl); 
-    cy.get('input[name="fname"]').type('John'); 
-    cy.get('input[name="lname"]').type('Mark Doe'); 
-    cy.get('input[type="submit"]').click(); 
-    cy.on('window:alert', str => { 
-        expect(str).to.equal('John Mark Doe'); 
-    }); 
-}
 
