74c9b8d1b4359dcef07dc011a46effbbf1b932dd
lokeshyaragarla <lokeshyaragarla@gmail.com>
2025-06-18 07:51:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function getFormvalue(event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-    // Get the trimmed values from the input fields
-    const firstName = document.querySelector('input[name="fname"]').value.trim();
-    const lastName = document.querySelector('input[name="lname"]').value.trim();
-    // Concatenate first name and last name
-    const fullName = `${firstName} ${lastName}`;
-    // Display the full name using alert
-    alert(fullName);
-}
+function getFormvalue() {
+    //Write your code here
 
+}
