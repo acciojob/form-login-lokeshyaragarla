eaad7eb34be66f8452b0b9ad3c4c639207ba50f4
lokeshyaragarla <lokeshyaragarla@gmail.com>
2025-06-19 08:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function getFormvalue(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
+  event.preventDefault();
 
   const firstName = document.getElementById('fname').value.trim();
   const lastName = document.getElementById('lname').value.trim();
 
-  // Check if fields are empty
   if (!firstName && !lastName) {
     alert("Please enter your full name.");
     return;
@@ -13,4 +12,3 @@ function getFormvalue(event) {
   const fullName = `${firstName} ${lastName}`.trim();
   alert(fullName);
 }
-
